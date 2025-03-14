bc633c00867001aab6422e98eaa56490562b2734
Wound Centrics <WoundCentrics@Mac.home>
2025-03-13 19:15:23 -0500

    Add .gitignore and Next.js generated files

---
+++ .next/server/app/api/content/route.js
@@ -0,0 +1,10 @@
+const CHUNK_PUBLIC_PATH = "server/app/api/content/route.js";
+const runtime = require("../../../chunks/[turbopack]_runtime.js");
+runtime.loadChunk("server/chunks/node_modules_next_0d16be08._.js");
+runtime.loadChunk("server/chunks/node_modules_esprima_dist_esprima_5db28383.js");
+runtime.loadChunk("server/chunks/node_modules_highlight_27a296ea.js");
+runtime.loadChunk("server/chunks/node_modules_d9c1a235._.js");
+runtime.loadChunk("server/chunks/[root of the server]__1c0c925d._.js");
+runtime.getOrInstantiateRuntimeModule("[project]/.next-internal/server/app/api/content/route/actions.js [app-rsc] (server actions loader, ecmascript)", CHUNK_PUBLIC_PATH);
+runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/content/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH);
+module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/content/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
